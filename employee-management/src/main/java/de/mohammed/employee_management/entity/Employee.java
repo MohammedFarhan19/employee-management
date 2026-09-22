c1ab5444632d22dc252ffeb4a1853bc2b23ec97a
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-22 14:10:19 +0200

    Use Lombok annotation

--- employee-management/src/main/java/de/mohammed/employee_management/entity/Employee.java
+++ employee-management/src/main/java/de/mohammed/employee_management/entity/Employee.java
@@ -3,6 +3,16 @@ package de.mohammed.employee_management.entity;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+/**
+ * using Lombok
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Employee {
 
 	private UUID id;
@@ -14,80 +24,5 @@ public class Employee {
 	private String postion;
 	private UUID departmentId;
 	
-	public Employee(UUID id, String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate,
-			String postion, UUID departmentId) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phoneNumber = phoneNumber;
-		this.hireDate = hireDate;
-		this.postion = postion;
-		this.departmentId = departmentId;
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public LocalDate getHireDate() {
-		return hireDate;
-	}
-
-	public void setHireDate(LocalDate hireDate) {
-		this.hireDate = hireDate;
-	}
-
-	public String getPostion() {
-		return postion;
-	}
-
-	public void setPostion(String postion) {
-		this.postion = postion;
-	}
-
-	public UUID getDepartmentId() {
-		return departmentId;
-	}
-
-	public void setDepartmentId(UUID departmentId) {
-		this.departmentId = departmentId;
-	}
 	
 }
