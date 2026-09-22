fd0ebf442d029e719d77f92ba7d3922d1239e93d
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-22 14:06:54 +0200

    create Entity Employee

---
+++ employee-management/src/main/java/de/mohammed/employee_management/entity/Employee.java
@@ -0,0 +1,93 @@
+package de.mohammed.employee_management.entity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Employee {
+
+	private UUID id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private LocalDate hireDate;
+	private String postion;
+	private UUID departmentId;
+	
+	public Employee(UUID id, String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate,
+			String postion, UUID departmentId) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.hireDate = hireDate;
+		this.postion = postion;
+		this.departmentId = departmentId;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(LocalDate hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getPostion() {
+		return postion;
+	}
+
+	public void setPostion(String postion) {
+		this.postion = postion;
+	}
+
+	public UUID getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(UUID departmentId) {
+		this.departmentId = departmentId;
+	}
+	
+}
